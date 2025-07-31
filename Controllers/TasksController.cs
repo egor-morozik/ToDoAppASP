e9e33c9820c53e1c3b7fc5ff5e354536050c4d38
egor-morozik <egor.mrz04@gmail.com>
2025-07-31 21:32:54 +0300

    - Add creat task and saw this taks after send form (all in Index action)

--- Controllers/TasksController.cs
+++ Controllers/TasksController.cs
@@ -5,10 +5,33 @@ namespace ToDoApp.Controllers
     public class TasksController : Controller
     {
         [HttpGet]
-        public async Task Index(string taskName)
+        public async Task Index()
+        {
+            string content = @"
+            <h2>Add New Task</h2>
+            <form method='post'>
+                <label>Description:</label><br/>
+                <input name='Description' required /><br/>
+                <label>Start At:</label><br/>
+                <input type='datetime-local' name='StartAt' required /><br/>
+                <label>End At:</label><br/>
+                <input type='datetime-local' name='EndAt' required /><br/>
+                <label>Is Active:</label><br/>
+                <input type='checkbox' name='IsActive' value='true' checked /><br/>
+                <input type='submit' value='Add Task' />
+            </form>";
+            Response.ContentType = "text/html;charset=utf-8";
+            await Response.WriteAsync(content);
+        }
+
+        [HttpPost]
+        public async Task Index(TaskItem task)
         {
             Response.ContentType = "text/html;charset=utf-8";
-            await Response.WriteAsync($"<h2>Current task: {taskName}</h2>");
+            await Response.WriteAsync(@$"<h2>Current task: {task.Description}</h2>
+                                        <h2>Created at: {task.StartAt}</h2>
+                                        <h2>End at: {task.EndAt}</h2>
+                                        <h2>Is active: {task.IsActive}</h2>");
         }
 
         [HttpPost]
@@ -23,7 +46,7 @@ namespace ToDoApp.Controllers
                             string Description,
                             DateTime StartAt,
                             DateTime EndAt,
-                            bool isActive
+                            bool IsActive
                             );
     }
 }
